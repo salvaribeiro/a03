fc50a6a064fcd4c13be241a6a9b06dbbfe02f7d4
Fábio Nogueira de Lucena <fabio@engenhariadesoftware.inf.br>
2014-04-22 11:56:06 -0300

    Acrescentado nova atividade para ilustrar novo leiaute, juntamente com opção de menu que a inicia. Laieute, contudo, ainda não inserido.

---
+++ src/br/ufg/inf/fs/android/a03/Leiaute1.java
@@ -0,0 +1,13 @@
+package br.ufg.inf.fs.android.a03;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by fabio_000 on 22/04/2014.
+ */
+public class Leiaute1 extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- src/br/ufg/inf/fs/android/a03/Principal.java
+++ src/br/ufg/inf/fs/android/a03/Principal.java
@@ -1,7 +1,10 @@
 package br.ufg.inf.fs.android.a03;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class Principal extends Activity {
     /**
@@ -11,5 +14,25 @@ public class Principal extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.principal, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.laieute1:
+                Intent intent = new Intent(this, Leiaute1.class);
+                startActivity(intent);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
