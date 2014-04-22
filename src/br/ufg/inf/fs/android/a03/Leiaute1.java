b49db892d531d7b2533b9c467905b9a8fe70ee14
Fábio Nogueira de Lucena <fabio@engenhariadesoftware.inf.br>
2014-04-22 12:58:48 -0300

    Leiaute relativo (RelativeLayout) ilustrado com botões e recuperação de uma página na web.

--- src/br/ufg/inf/fs/android/a03/Leiaute1.java
+++ src/br/ufg/inf/fs/android/a03/Leiaute1.java
@@ -2,6 +2,7 @@ package br.ufg.inf.fs.android.a03;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 /**
  * Created by fabio_000 on 22/04/2014.
@@ -9,5 +10,8 @@ import android.os.Bundle;
 public class Leiaute1 extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.layoute1);
+        WebView web = (WebView) findViewById(R.id.webView);
+        web.loadUrl("http://www.inf.ufg.br");
     }
 }
